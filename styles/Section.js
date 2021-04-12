365b9e21fa697318a8e076104874a28ee2a0b452
AhuiVargas <ahuijr@gmail.com>
2021-04-12 00:41:53 -0500

    fix

--- styles/Section.js
+++ styles/Section.js
@@ -5,23 +5,12 @@ const { colors } = theme;
 const Section = styled.section`
   padding: 3rem 5rem;
 
-  ${media.bp900`
-    padding: 2rem;
-  `};
-  ${media.bp400`
-    padding: 1rem;
-  `};
-
   ${(props) =>
     props.dark &&
     css`
       background-color: ${colors.black};
       color: ${colors.lightestBlue};
       padding-bottom: 10rem;
-      ${media.bp900`
-        padding-top: 2rem;
-        padding-bottom: 10rem;
-      `};
     `};
 
   & > div {
